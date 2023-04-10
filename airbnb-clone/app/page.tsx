d287e3be3768d984706a8d976f07895f9dea280d
n8fury <mhjoy547@yahoo.com>
2023-04-10 16:06:43 +0600

    starter app skeleton

---
+++ airbnb-clone/app/page.tsx
@@ -0,0 +1,9 @@
+import { Inter } from 'next/font/google';
+import Image from 'next/image';
+import styles from './page.module.css';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export default function Home() {
+	return <div>Hello Airbnb!</div>;
+}
