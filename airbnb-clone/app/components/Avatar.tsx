31d5234b2394b88e695ce44cfe60612f7656f061
n8fury <mhjoy547@yahoo.com>
2023-04-16 23:22:47 +0600

    added avatarcomponent and fixed font issue

---
+++ airbnb-clone/app/components/Avatar.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import Image from 'next/image';
+
+const Avatar = () => {
+	return (
+		<div>
+			<Image
+				alt='Avatar'
+				className='rounded-full'
+				height='30'
+				width='30'
+				src='/Images/avatar.jpg'
+			/>
+		</div>
+	);
+};
+
+export default Avatar;
+    
--- airbnb-clone/app/components/navbar/UserMenu.tsx
+++ airbnb-clone/app/components/navbar/UserMenu.tsx
@@ -1,12 +1,13 @@
 'use client';
 import { AiOutlineMenu } from 'react-icons/ai';
+import Avatar from '../Avatar';
 const UserMenu = () => {
 	return (
 		<div className='relative'>
 			<div className='flex flex-row items-center gap-3'>
 				<div
 					onClick={() => {}}
-					className='hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer'
+					className='hidden md:block text-sm font-bold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer'
 				>
 					Airbnb your home
 				</div>
@@ -15,6 +16,9 @@ const UserMenu = () => {
 					className=' p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition '
 				>
 					<AiOutlineMenu />
+					<div className='hidden md:block'>
+						<Avatar />
+					</div>
 				</div>
 			</div>
 		</div>
