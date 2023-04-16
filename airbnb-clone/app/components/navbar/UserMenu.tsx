b77209d4ae4582c3956d64f3734b07456d2d539f
n8fury <mhjoy547@yahoo.com>
2023-04-17 00:07:29 +0600

    added menu-item component

--- airbnb-clone/app/components/navbar/UserMenu.tsx
+++ airbnb-clone/app/components/navbar/UserMenu.tsx
@@ -1,7 +1,13 @@
 'use client';
+import { useCallback, useState } from 'react';
 import { AiOutlineMenu } from 'react-icons/ai';
 import Avatar from '../Avatar';
+import MenuItem from './MenuItem';
 const UserMenu = () => {
+	const [isOpen, setIsOpen] = useState(false);
+	const toggleOpen = useCallback(() => {
+		setIsOpen((value) => !value);
+	}, []); //TODO: work on this
 	return (
 		<div className='relative'>
 			<div className='flex flex-row items-center gap-3'>
@@ -12,7 +18,7 @@ const UserMenu = () => {
 					Airbnb your home
 				</div>
 				<div
-					onClick={() => {}}
+					onClick={toggleOpen}
 					className=' p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition '
 				>
 					<AiOutlineMenu />
@@ -21,6 +27,16 @@ const UserMenu = () => {
 					</div>
 				</div>
 			</div>
+			{isOpen && (
+				<div className='absolute rounded-xl shadow-md w-[400vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm'>
+					<div className='flex flex-col cursor-pointer'>
+						<>
+							<MenuItem onClick={() => {}} label='login' />
+							<MenuItem onClick={() => {}} label='Signup' />
+						</>
+					</div>
+				</div>
+			)}
 		</div>
 	);
 };
