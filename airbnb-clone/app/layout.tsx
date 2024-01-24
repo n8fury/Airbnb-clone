875edce3e7d6483e2f46b3d0ed6c386c6563f96d
n8fury <mhjoy547@yahoo.com>
2024-01-25 01:39:45 +0600

    added modals

--- airbnb-clone/app/layout.tsx
+++ airbnb-clone/app/layout.tsx
@@ -1,33 +1,35 @@
 import { Analytics } from '@vercel/analytics/react';
 import { Nunito } from 'next/font/google';
 import ClientOnly from './components/ClientOnly';
+import Modal from './components/modals/Modal';
 import Navbar from './components/navbar/Navbar';
 import './globals.css';
 
 export const metadata = {
-	title: 'Airbnb',
-	description: 'Airbnb-clone by next.js',
-	content: 'nGVM0iXqB439f-HiPJPIbfkzy9n33-JVzB3mdk6ojFE',
+  title: 'Airbnb',
+  description: 'Airbnb-clone by next.js',
+  content: 'nGVM0iXqB439f-HiPJPIbfkzy9n33-JVzB3mdk6ojFE',
 };
 
 const font = Nunito({
-	subsets: ['latin'],
+  subsets: ['latin'],
 });
 
 export default function RootLayout({
-	children,
+  children,
 }: {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-	return (
-		<html lang='en'>
-			<body className={font.className}>
-				<ClientOnly>
-				<Navbar />
-				</ClientOnly>
-				{children}
-				{/* <Analytics /> */}
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        {/* <ClientOnly> */}
+        <Modal title="hello world" isOpen />
+        <Navbar />
+        {/* </ClientOnly> */}
+        {children}
+        {/* <Analytics /> */}
+      </body>
+    </html>
+  );
 }
